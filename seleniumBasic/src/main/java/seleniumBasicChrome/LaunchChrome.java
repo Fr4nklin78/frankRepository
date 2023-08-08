c286c2457fa144f80ba63c34445fe6f9798f7061
Fr4nklin78 <franklin.i.rock@gmail.com>
2023-08-08 11:18:36 +0530

    b

--- seleniumBasic/src/main/java/seleniumBasicChrome/LaunchChrome.java
+++ seleniumBasic/src/main/java/seleniumBasicChrome/LaunchChrome.java
@@ -1,15 +1,18 @@
 package seleniumBasicChrome;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class launchChrome {
+public class LaunchChrome {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webDriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+		driver.get("https://letcode.in");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		String title = driver.getTitle();
 		System.out.println(title);
 		driver.quit();
